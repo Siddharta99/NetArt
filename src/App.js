879015facc1db055f0dbedfd284f7824ab29f2da
Siddharta99 <siddhartawakte@gmail.com>
2024-02-28 13:54:12 +0530

    Second

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 // App.js
 import React from 'react';
 import ImageGallery from './ImageGallery';
-import text from './text';
+
 
 const App = () => {
   return (
     <div className="App">
-      <h1></h1>
+      
       <ImageGallery img />
-      <text  />
+      
     </div>
   );
 };
